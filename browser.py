65a41777848ae25d598de092542b18f4ebaf92c8
chand1012 <chandler@multisiglabs.org>
2023-04-18 15:26:02 -0400

    Move driver loading

--- browser.py
+++ browser.py
@@ -3,11 +3,10 @@ import time
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 
-options = Options()
-options.headless = True
-driver = webdriver.Firefox(options=options)
-
 def get_site(url: str):
+    options = Options()
+    options.headless = True
+    driver = webdriver.Firefox(options=options)
     driver.get(url)
     time.sleep(2)
     return driver.page_source

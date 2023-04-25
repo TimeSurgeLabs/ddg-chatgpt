c32bc0ebaf1e31231255abde264c70512318c2ec
chand1012 <chandler@multisiglabs.org>
2023-04-25 10:04:10 -0400

    Add robots file

--- main.py
+++ main.py
@@ -17,19 +17,23 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 @app.get("/")
 def index():
     return {"message": "Hello World"}
+
 
 @app.post("/search", description="Search for a query", response_model=SearchResponse)
 def search(search_req: SearchRequest):
     results = ddg(search_req.query)
     return {"results": results}
 
+
 @app.post('/news', description="Get the latest news", response_model=SearchResponse)
 def news(search_req: SearchRequest):
     results = ddg_news(search_req.query)
     return {"results": results}
+
 
 @app.post("/content", description="Get the content of a URL", response_model=ContentResponse)
 def content(content_req: ContentRequest):
@@ -37,7 +41,14 @@ def content(content_req: ContentRequest):
     text = get_text(resp)
     return {"content": text}
 
+
 @app.get('/.well-known/ai-plugin.json', include_in_schema=False)
 def read_ai_plugin_json() -> Response:
     with open('ai-plugin.json', 'r') as f:
         return Response(f.read(), media_type='application/json')
+
+
+@app.get('/robots.txt', include_in_schema=False)
+def read_robots_txt() -> Response:
+    with open('robots.txt', 'r') as f:
+        return Response(f.read(), media_type='text/plain')
